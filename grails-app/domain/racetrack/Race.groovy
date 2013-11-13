b7a11ff9ccefea09b091c1b74bcc411f635dd008
Sushil Dahal <sudahal@DWNPCPU043.(none)>
2013-11-13 11:20:57 +0545

    Initial Commit

---
+++ grails-app/domain/racetrack/Race.groovy
@@ -0,0 +1,21 @@
+package racetrack
+
+class Race {
+	String name
+	Date startDate
+	String city
+	String state
+	BigDecimal distance
+	BigDecimal cost
+	Integer maxRunners = 100000
+	
+    static constraints = {
+    	name(blank:false, maxSize:50)
+    	startDate()
+    	city()
+    	state(inList:["KTM", "MAK", "CIT", "BRT"])
+    	distance(min: 0.0)
+    	cost(min:0.0, max:100.0)
+    	maxRunners(min:0, max:100000)
+    }
+}
---
+++ grails-app/domain/racetrack/Registration.groovy
@@ -0,0 +1,16 @@
+package racetrack
+
+class Registration {
+	String name
+	Date dateOfBirth
+	String gender
+	String address
+	String city
+	String state
+	String zipcode
+	String email
+	Date dateCreated //Special name
+	
+    static constraints = {
+    }
+}
